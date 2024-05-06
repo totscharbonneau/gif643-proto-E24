23acbad0a273929af5a85161b5e1657f198df1f6
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2024-05-06 17:40:48 -0400

    Added other required perf tests

--- perf_compare.sh
+++ perf_compare.sh
@@ -2,17 +2,19 @@ process_runner () {
     for i in $(seq 1 $ITERATORNUMBER)
     do
         INDEX=$(($i - 1))
-        echo "${TASKSARRAY[$INDEX]}" | ./asset_conv - 1 2> /dev/null &
+        echo "${TASKSARRAY[$INDEX]}" | ./asset_conv - $ITERATORPROCESSNUMBER 2> /dev/null &
     done
     wait
 }
 
 
 ITERATORNUMBER="${1:-4}"
+echo "Time running asset_conv with 1 thread:"
+time ./asset_conv tasks.csv 1 2> /dev/null
+
 echo "Time running asset_conv with $ITERATORNUMBER threads:"
 time ./asset_conv tasks.csv $ITERATORNUMBER 2> /dev/null
 
-echo "Time running asset_conv with $ITERATORNUMBER processes:"
 NUMBEROFLINES=$(wc -l < tasks.csv)
 if [[ $(($NUMBEROFLINES % $ITERATORNUMBER)) = 0 ]]
 then
@@ -21,6 +23,11 @@ then
     do
         TASKSARRAY+=("$(head -$(($i * $LINESDIV)) tasks.csv | tail +$(($i * $LINESDIV - $LINESDIV + 1)))")
     done
+    ITERATORPROCESSNUMBER=1
+    echo "Time running asset_conv with $ITERATORNUMBER processes and 1 thread per process:"
+    time process_runner
+    ITERATORPROCESSNUMBER="${2:-4}"
+    echo "Time running asset_conv with $ITERATORNUMBER processes and $ITERATORPROCESSNUMBER threads per process:"
     time process_runner
 else
     echo "tasks.csv needs to be divisable by $ITERATORNUMBER"
